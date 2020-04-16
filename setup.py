e70453e723974b69017b886b3aec7e6036162a31
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-04-16 20:04:21 +0100

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
 		name="domdf_spreadsheet_tools",
-		version="0.1.3",
+		version="0.1.4",
 		author='Dominic Davis-Foster',
 		author_email="dominic@davis-foster.co.uk",
 		py_modules=["domdf_spreadsheet_tools"],
