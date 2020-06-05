aeafe842304f53f62be9cb40705c046c67277bad
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-06-05 11:57:23 +0100

    Bump version: 0.1.5 → 0.1.6

--- domdf_spreadsheet_tools.py
+++ domdf_spreadsheet_tools.py
@@ -45,7 +45,7 @@ __author__ = "Dominic Davis-Foster"
 __copyright__ = "Copyright 2018-2019 Dominic Davis-Foster"
 
 __license__ = "LGPL"
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __email__ = "dominic@davis-foster.co.uk"
 
 
